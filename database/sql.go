e2de49665b18f4fc9b57bda2a5f60345464d112e
nj-jay <442613776@qq.com>
2020-11-12 18:40:43 +0800

    sql语句

--- database/sql.go
+++ database/sql.go
@@ -8,16 +8,19 @@ var (
 	//查询数据表login登录用户的信息
 	QueryLogin = `SELECT *FROM login`
 
-	//单例查询by id
-	QuerySingleDataById = `SELECT *FROM test WHERE id = ?;`
+	//单例查询by name前缀
+	QueryDataByName = `SELECT *FROM test WHERE name like ?;`
 
 	//单例删除by id
 	DeleteSingleDataById = `DELETE FROM test WHERE id = ?;`
 
 	//单例更新 price by id
-	UpdateSingleDataById = `UPDATE test SET price = ? WHERE id = ?;`
+	UpdateSingleDataById = `UPDATE test SET name = ?, price = ? WHERE id = ?;`
 
 	//单例add
 	PostSingleData = `insert into test (name, price) values(?,?);`
+
+	//添加用户
+	PostUsername = `insert into login (username, password) values(?,?);`
 
 )
