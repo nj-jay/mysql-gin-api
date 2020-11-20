4bc18d3a2f1a84a3624126ee748477fe56f870d7
nj-jay <442613776@qq.com>
2020-11-20 14:00:23 +0800

    sql语句v2与连接数据库

--- database/sql.go
+++ database/sql.go
@@ -2,23 +2,23 @@ package database
 
 var (
 
-	//查询数据表test中的所有数据
-	QueryAllData = `SELECT *FROM test;`
+	//查询数据表test中的所有数据,根据前段传入的page进行分页展示
+	QueryAllData = `SELECT *FROM bookManage limit ?, 8;`
 
 	//查询数据表login登录用户的信息
-	QueryLogin = `SELECT *FROM login`
+	QueryLogin = `SELECT * FROM login`
 
-	//单例查询by name前缀
-	QueryDataByName = `SELECT *FROM test WHERE name like ?;`
+	//单例查询by name or types or author进行模糊匹配
+	QueryDataByName = `SELECT * FROM bookManage WHERE concat(name, types, author) REGEXP ?;`
 
 	//单例删除by id
-	DeleteSingleDataById = `DELETE FROM test WHERE id = ?;`
+	DeleteSingleDataById = `DELETE FROM bookManage WHERE id = ?;`
 
 	//单例更新 price by id
-	UpdateSingleDataById = `UPDATE test SET name = ?, price = ? WHERE id = ?;`
+	UpdateSingleDataById = `UPDATE bookManage SET name = ?, types = ?, author = ?, price = ?, addTime = ? WHERE id = ?;`
 
 	//单例add
-	PostSingleData = `insert into test (name, price) values(?,?);`
+	PostSingleData = `insert into bookManage (name, types, author, price, addTime) values(?,?,?,?,?);`
 
 	//添加用户
 	PostUsername = `insert into login (username, password) values(?,?);`
