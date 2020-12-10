300673866600f90ebde0c3c77f46636031ff8a32
nj-jay <442613776@qq.com>
2020-12-10 15:13:50 +0800

    upgrade main.go

--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 
+	"github.com/gin-gonic/gin"
+	"github.com/nj-jay/httpServer/middlewares"
 	"github.com/nj-jay/httpServer/routers"
-
 	"log"
 
 )
 
 func main() {
 
-	r := routers.Router()
+	//r := routers.Router()
+
+	r := gin.Default()
+
+	r.Use(middlewares.Cors())
+
+	routers.LoadLogin(r)
+
+	routers.LoadBooks(r)
 
 	err := r.Run(":8080")
 
